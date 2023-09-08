439dc7911e5923112f6ac047725b4f9d4ca28364
jiale qiu <jiale98chinoguay@gmail.com>
2023-09-07 22:11:50 -0400

    [Install]: nodemon dev package

--- app.js
+++ app.js
@@ -2,12 +2,10 @@ const express = require("express");
 const app = express();
 
 app.get('/', (req, res) => {
-    res.status(200);
-    res.send("Welcome to jfinance!~");
+    res.send("<h1>Welcome to jfinance!~</h1>");
 });
 
 const port = 4000;
 const server = app.listen(port);
-
 
 console.log(`Listening on port ${port}...`)
