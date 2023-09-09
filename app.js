44e627b47772eeee23039cacbab58933ee9ba6d0
jiale qiu <jiale98chinoguay@gmail.com>
2023-09-08 23:32:40 -0400

    [Setup]: swagger endpoints

--- app.js
+++ app.js
@@ -6,6 +6,22 @@ app.get('/', (req, res) => {
 });
 
 const port = 4000;
-const server = app.listen(port);
 
-console.log(`Listening on port ${port}...`)
+// Setup Swagger
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// Routes
+app.get(`/api/todos`, (req, res) => {
+    console.log("get all todos!!");
+    res.json({
+        "id": 1,
+        "task": "call dentist",
+        "status": "incomplete"
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}...`);
+});
